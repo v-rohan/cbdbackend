7ec7dc95bf93481f4cd0538344df69ae2f435c23
v-rohan <rohanrao.dec11@gmail.com>
2022-03-11 04:09:09 +0530

    snw

--- src/routes/sneRoutes.ts
+++ src/routes/sneRoutes.ts
@@ -41,7 +41,7 @@ module.exports = (app: Express, passport) => {
                 sne.shortlink = link;
                 console.log(request.body);
                 let domain = new URL(request.body.mainlink).hostname;
-                domain = 'https://' + domain;
+                domain = 'https://' + domain + '/';
                 console.log(domain);
                 sne.store = await getRepository(Store).findOneOrFail({
                     where: { homepage: domain },
