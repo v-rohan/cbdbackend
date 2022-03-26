edc1b3be5bc71ef36037138670c7149f26c8d5f9
mitraditya <am.19u10524@btech.nitdgp.ac.in>
2022-03-26 17:35:13 +0530

    Changed referral settings route.

--- src/routes/settingsRoutes.ts
+++ src/routes/settingsRoutes.ts
@@ -10,6 +10,23 @@ module.exports = (app: Express, passport) => {
     require("../passport/jwt")(passport);
     require("../passport/google")(passport);
 
+    app.post(
+        "/settings",
+        passport.authenticate("jwt", { session: false }),
+        AdminCheck,
+        async (req: IGetUserAuthInfoRequest, res: Response) => {
+            try {
+                var settings = new Settings();
+                settings = { ...req.body };
+                await getRepository(Settings).save(settings);
+                res.status(200).send(settings);
+            } catch (e) {
+                console.log(e);
+                res.status(500).send(e);
+            }
+        }
+    );
+
     app.put(
         "/settings/:id",
         passport.authenticate("jwt", { session: false }),
@@ -45,9 +62,7 @@ module.exports = (app: Express, passport) => {
                 const settings = await getRepository(Settings).find();
                 res.set({
                     "Access-Control-Expose-Headers": "Content-Range",
-                    "Content-Range": `X-Total-Count: ${1} - ${settings.length} / ${
-                        settings.length
-                    }`,
+                    "Content-Range": `X-Total-Count: 1`,
                 });
                 res.status(200).send(settings);
             } catch (e) {
