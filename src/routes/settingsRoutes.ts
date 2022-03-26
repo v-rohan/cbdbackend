45e7568ee42e39af01784c6df827f207dc53aabe
mitraditya <am.19u10524@btech.nitdgp.ac.in>
2022-03-26 16:56:15 +0530

    Changed referral settings route.

--- src/routes/settingsRoutes.ts
+++ src/routes/settingsRoutes.ts
@@ -10,23 +10,28 @@ module.exports = (app: Express, passport) => {
     require("../passport/jwt")(passport);
     require("../passport/google")(passport);
 
-    app.post(
-        "/settings",
+    app.put(
+        "/settings/:id",
         passport.authenticate("jwt", { session: false }),
         AdminCheck,
         async (req: IGetUserAuthInfoRequest, res: Response) => {
+            var settings = new Settings();
             try {
-                var settings = new Settings();
-                settings = getRepository(Settings).find()[0];
-                if (!settings) {
-                    settings = new Settings();
-                }
+                settings = await getRepository(Settings).findOneOrFail(
+                    req.params.id
+                );
                 settings = { ...settings, ...req.body };
-                await getRepository(Settings).save(settings);
-                res.status(200).send(settings);
-            } catch (e) {
-                console.log(e);
-                res.status(500).send(e);
+                getRepository(Settings)
+                    .save(settings)
+                    .then((settings) => {
+                        res.status(201).send(settings);
+                    })
+                    .catch((error) => {
+                        res.status(400).send(error);
+                    });
+            } catch (error) {
+                console.log(error);
+                res.status(500).send(error);
             }
         }
     );
@@ -37,8 +42,7 @@ module.exports = (app: Express, passport) => {
         AdminCheck,
         async (req: IGetUserAuthInfoRequest, res: Response) => {
             try {
-                var settings = new Settings();
-                settings = getRepository(Settings).find()[0];
+                const settings = await getRepository(Settings).find();
                 res.set({
                     "Access-Control-Expose-Headers": "Content-Range",
                     "Content-Range": `X-Total-Count: 1`,
